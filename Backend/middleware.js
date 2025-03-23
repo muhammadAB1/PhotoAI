610712381fdf0695fee150f161968c4a4a5cdd60
muhammadAB1 <abrarjpj7@gmail.com>
2025-03-23 16:22:20 +0500

    removing jose

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,25 +1,22 @@
-import { jwtVerify } from 'jose';
+import jwt from 'jsonwebtoken'
 
-export async function authMiddleware(req, res, next) {
+export function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization'];
 
-    const token = authHeader?.split(' ')[1];
+    const token = authHeader?.split(' ')[1]
 
     try {
-        const secret = process.env.AUTH_JWT_TOKEN;
-        const decoded = await jwtVerify(token, secret, {
-            algorithms: ['RS256']
-        });
+        const decoded = jwt.decode(token, process.env.AUTH_JWT_TOKEN, {
+            algoithms: ['RS256']
+        })
 
-        if (decoded.sub) {
-            req.userId = decoded.sub;
+        if(decoded?.sub) {
+            req.userId = decoded?.sub
             next();
-        } else {
-            throw new Error('Invalid token');
         }
     } catch (error) {
         res.status(403).json({
             message: 'Error while decoding'
-        });
+        })
     }
 }
